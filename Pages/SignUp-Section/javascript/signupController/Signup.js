aef5a5ad8323a4de0adc377e49f5c67a895257ae
Basil <basilabushihab22@gmail.com>
2024-06-29 17:34:22 +0300

    fixed login signup bugs and edited firebase config file

--- Pages/SignUp-Section/javascript/signupController/Signup.js
+++ Pages/SignUp-Section/javascript/signupController/Signup.js
@@ -1,28 +1,30 @@
 import {
   auth,
   createUser,
-  setData,
+  addData,
   database,
-  getData,
+  retrieveData,
   sendEmailLink,
-} from "../../../../Firebase-config/firebase-config";
-
+} from "../../../../Firebase-config/firebase-config.js";
 
 function signup(user, password) {
+  console.log(password);
+  console.log(user);
   createUser(auth, user.email, password)
     .then((userCredential) => {
-      sendEmailLink(auth.createUser).then(() => {
+      sendEmailLink(auth.currentUser).then(() => {
         alert(
           "an email verification link has been sent to this account please check your inbox in order to login"
         );
-        location.href="../../Login-Section/Login.html";
       });
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
+
+      console.log(errorCode);
+      console.log(errorMessage);
     });
 }
-
 
 export { signup, createUser };
